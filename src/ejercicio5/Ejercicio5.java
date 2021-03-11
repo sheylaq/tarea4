28c5f5ce21b24e59769369d9a0a206f2b2c52a28
sheylaq <80442391+sheylaq@users.noreply.github.com>
2021-03-10 23:40:13 -0600

    primer commit

---
+++ src/ejercicio5/Ejercicio5.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio5;
+
+/**
+ *
+ * @author user
+ */
+public class Ejercicio5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        for(int x=2;x<=100; x=x+2){
+            System.out.println(x+" \t");
+        }
+    }
+    
+}
